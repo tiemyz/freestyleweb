1fffb08e6e28d6f94add10aae63ada1f6e15e8dc
tiemyz <rm95145@fiap.com.br>
2023-09-04 21:40:24 -0300

    📑Fetch

---
+++ src/app/contas/DataRow.jsx
@@ -0,0 +1,19 @@
+import DropMenu from "@/components/DropMenu";
+import { BanknotesIcon } from '@heroicons/react/24/outline'
+
+export default function DataRow({conta}) {
+    return (
+        <div id="data-row" className="group flex items-center justify-between p-2 my-2 rounded cursor-pointer hover:bg-red-600">
+            <div className="flex gap-2 items-center">
+                <BanknotesIcon className="h-6 w-6" />
+                <span>{conta.nome}</span>
+            </div>
+            <div className="flex items-center">
+                <span>R$ 100.00</span>
+                <div className="invisible group-hover:visible">
+                    <DropMenu />
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/contas/page.js
+++ src/app/contas/page.js
@@ -1,12 +1,23 @@
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
-export default function Contas() {
+async function getContas(){
+  const url = "http://localhost:8080/api/v1/conta"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Contas() {
+  const contas = await getContas()
   return (
     <>
       <NavBar active={"contas"} />
 
-      <main className="bg-black m-20 p-12">
-        <h2>Contas</h2>
+      <main className="bg-slate-900 mt-10 p-9 rounded max-w-xl m-auto">
+        <h2 className="text-slate-100 text-2xl">Contas</h2>
+        <div id="data">
+         {contas.map(conta => <DataRow key={conta.id} conta={conta} />)}
+        </div>
       </main>
     </>
 
